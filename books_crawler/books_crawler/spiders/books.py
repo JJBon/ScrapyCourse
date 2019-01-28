c0ef61ad191abc1deef2621cf8ee2a4b8d62dcce
Juan Jose Bonilla <jjbonillatroncoso@gmail.com>
2019-01-28 13:31:21 -0500

    created spider without selenium

--- books_crawler/books_crawler/spiders/books.py
+++ books_crawler/books_crawler/spiders/books.py
@@ -57,4 +57,27 @@ from time import sleep
 #     def parse_book(self,response):
 #         pass
 
+class BooksSpider(Spider):
+    name = 'books'
+    allowed_domains = ['books.toscrape.com']
+    start_urls      = ['http://books.toscrape.com']
+
+   
+    
+    def parse(self,response):
+        books = response.xpath('//h3/a/@href').extract()
+        for book in books:
+            absolute_url = response.urljoin(book)
+            yield Request(absolute_url,callback=self.parse_book)
+
+        #process next page
+        next_page_url = response.xpath('//a[text()="next"]/@href').extract_first()
+        absolute_next_page_url = response.urljoin(next_page_url)
+        yield Request(absolute_next_page_url)
+    
+    def parse_book(self,response):
+        pass
+        
+        
+
 
